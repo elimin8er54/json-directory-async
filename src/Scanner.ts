99bb27cd87163421fa4eccc6c898d890477a1eba
= <shaunt.keshishian@gmail.com>
2021-09-22 19:25:28 -0400

    Github Workflow

--- src/Scanner.ts
+++ src/Scanner.ts
@@ -5,7 +5,7 @@ import { Util, Callback, Documents, ItemStats, Options, Type } from "./Util";
 
 //Thank you Typescript for forcing me to reformat the code properly with all the errors it gives me.
 
-export default class Scanner {
+module.exports = class Scanner {
 
     showSymbolicLink: boolean = false;
     showDepth: boolean = false;
